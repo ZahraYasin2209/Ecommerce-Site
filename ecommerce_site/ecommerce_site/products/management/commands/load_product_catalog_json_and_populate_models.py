c46474c4a7f5b6b275a9e5db5e6950d2e7f79caa
Zahra Yasin <zahrayasin2209@gmail.com>
2025-12-01 14:53:38 +0500

    Updated Management Command

--- ecommerce_site/ecommerce_site/products/management/commands/load_product_catalog_json_and_populate_models.py
+++ ecommerce_site/ecommerce_site/products/management/commands/load_product_catalog_json_and_populate_models.py
@@ -23,12 +23,11 @@ from .mappings import (
 
 
 class Command(BaseCommand):
-     help = (
-        "Reads product information from clothes.json and stores it across all product-related "
-        "models in the database. This includes automatically detecting and assigning categories,"
-        "creating or updating products, generating product details, saving product images, "
-        "leaning price formats, and performing all operations inside an atomic transaction to "
-        "maintain data integrity."
+    help = (
+        "Reads product information from clothes.json and stores it across all product-related models "
+        "in the database. This includes automatically detecting and assigning categories, creating or "
+        "updating products, generating product details, saving product images, cleaning price formats, "
+        "and performing all operations inside an atomic transaction to maintain data integrity."
     )
 
     def get_category_from_product_name(self, product_name):
@@ -47,8 +46,7 @@ class Command(BaseCommand):
         found_product_material = "N/A"
 
         for product_material in PRODUCT_MATERIALS:
-            if any(product_material in product_attribute
-                   for product_attribute in product_details):
+            if any(product_material in product_attribute for product_attribute in product_details):
                 found_product_material = product_material
 
         return found_product_material
