44daeedd19e97fc89d719c842622272442a3b4a9
Zahra Yasin <zahrayasin2209@gmail.com>
2025-12-03 13:04:08 +0500

    Updated

--- ecommerce_site/ecommerce_site/ecommerce_site/urls.py
+++ ecommerce_site/ecommerce_site/ecommerce_site/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from .views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls', namespace='products')),
     path('', home, name='home'),
+    path("cart/", include("orders.urls", namespace="cart")),
 ]
