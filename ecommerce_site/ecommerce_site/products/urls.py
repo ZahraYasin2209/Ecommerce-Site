b613c838019108e6f574906802a7067bfaa9b290
Zahra Yasin <zahrayasin2209@gmail.com>
2025-12-03 11:16:38 +0500

    Views and Templates created for Products app

---
+++ ecommerce_site/ecommerce_site/products/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views.cart import add_to_cart
+from .views.product_detail import ProductDetailView
+from .views.product_list import ProductListView
+
+
+app_name = "products"
+
+urlpatterns = [
+    path("", ProductListView.as_view(), name="list"),
+    path("category/<int:category_id>/", ProductListView.as_view(), name="category_products"),
+    path("<int:pk>/", ProductDetailView.as_view(), name="detail"),
+    path("<int:product_pk>/add-to-cart/", add_to_cart, name="add_to_cart"),
+]
---
+++ ecommerce_site/ecommerce_site/products/views/constants.py
@@ -0,0 +1,6 @@
+PRODUCT_ORDER_MAPPING = {
+    "price_low_to_high": "min_price",
+    "price_high_to_low": "-min_price",
+    "newest_first": "-created",
+    "oldest_first": "created",
+}
