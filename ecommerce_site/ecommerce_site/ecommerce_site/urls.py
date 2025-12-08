5e169862f1311e9f7936628870219d4541b62bc7
Zahra Yasin <109916495+ZahraYasin2209@users.noreply.github.com>
2025-12-08 17:28:39 +0500

    Clean up imports in urls.py
    Removed redundant import of 'os' in urls.py.

--- ecommerce_site/ecommerce_site/ecommerce_site/urls.py
+++ ecommerce_site/ecommerce_site/ecommerce_site/urls.py
@@ -1,8 +1,7 @@
+import os
+
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-import os
-from django.conf import settings
 
 from .views import home
 
