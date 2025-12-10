96b1fc18983fb3bd99778ef01d6a724f331e103a
Zahra Yasin <zahrayasin2209@gmail.com>
2025-12-10 15:29:19 +0500

    Dashboard Implementation

--- ecommerce_site/ecommerce_site/ecommerce_site/urls.py
+++ ecommerce_site/ecommerce_site/ecommerce_site/urls.py
@@ -16,8 +16,3 @@ urlpatterns = [
     path("users/", include("users.urls", namespace="account")),
     path('admin_dashboard/', include('dashboard.urls', namespace='dashboard')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'ecommerce_site/static'))
-
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
